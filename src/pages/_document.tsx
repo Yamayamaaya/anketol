7f65eeaa686493030e999da5cbe6f8ffc3a6bb88
Yamayamaaya <yugo139013@gmail.com>
2023-12-20 12:00:54 +0900

    OGP修正　htmlタグ

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   override render() {
     return (
-      <Html>
+      <Html lang="ja" prefix="og: http://ogp.me/ns#">
         <Head>
           <meta property="og:title" content="アンケトル" />
           <meta
