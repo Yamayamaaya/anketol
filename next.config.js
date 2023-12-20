ed2fc06adc2811578b887136fa08d8e5b2bb64ee
Yamayamaaya <yugo139013@gmail.com>
2023-12-20 10:47:07 +0900

    諸々修正
    https://blog.bltinc.co.jp/entry/2022/11/16/150000

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   experimental: { newNextLinkBehavior: false },
+  images: {
+    unoptimized: true, // 画像最適化を行わない
+  },
 }
-
 module.exports = nextConfig
