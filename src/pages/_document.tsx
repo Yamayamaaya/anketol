df1cef0bedb7748be7e0819d6e80505d14a56b0a
Yamayamaaya <yugo139013@gmail.com>
2023-12-20 11:15:10 +0900

    OGP設定

---
+++ src/pages/_document.tsx
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  override render() {
+    return (
+      <Html>
+        <Head>
+          <title>Anketol</title>
+          <link rel="icon" href="/favicon.png " />
+          <meta property="og:title" content="アンケトル" />
+          <meta
+            property="og:description"
+            content="アンケートを、もっと手軽に"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://anketol.web.app/signin" />
+          <meta property="og:image" content="/favicon.png" />
+          <meta property="og:site_name" content="アンケトル" />
+          <meta property="og:locale" content="ja_JP" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
