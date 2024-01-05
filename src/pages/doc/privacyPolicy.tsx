dbbc459b3eafc4f2c33ddf9517c800afcf3696d9
Yamayamaaya <yugo139013@gmail.com>
2024-01-05 22:03:39 +0900

    Revert "ogp設定、試し"
    This reverts commit e25dfd047b3804c247644bc6868d13d83a604616.

--- src/pages/doc/privacyPolicy.tsx
+++ src/pages/doc/privacyPolicy.tsx
@@ -1,15 +1,10 @@
 import { MarkdownContainer } from '@src/components/MarkdownContainer'
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
-import type { GetStaticProps } from 'next'
 
-export const getStaticProps: GetStaticProps = async () => {
-  const ogTitle = 'プライバシーポリシー' // ここでOGPタイトルを取得
-  return { props: { ogTitle } }
-}
-
-const PrivacyPolicy: React.FC<{ ogTitle: string }> = ({ ogTitle }) => {
+const PrivacyPolicy = () => {
   const [markdown, setMarkdown] = useState('')
+
   useEffect(() => {
     const fetchMarkdown = async () => {
       const res = await fetch('/doc/privacy-policy.md')
@@ -21,8 +16,8 @@ const PrivacyPolicy: React.FC<{ ogTitle: string }> = ({ ogTitle }) => {
   return (
     <>
       <Head>
-        <title>{ogTitle}</title>
-        <meta property="og:title" content={ogTitle} />
+        <title>プライバシーポリシー</title>
+        <meta property="og:title" content="プライバシーポリシー" />
       </Head>
       <MarkdownContainer markdown={markdown} />
     </>
