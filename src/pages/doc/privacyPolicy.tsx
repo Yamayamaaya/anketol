e25dfd047b3804c247644bc6868d13d83a604616
Yamayamaaya <yugo139013@gmail.com>
2024-01-05 21:45:59 +0900

    ogp設定、試し

--- src/pages/doc/privacyPolicy.tsx
+++ src/pages/doc/privacyPolicy.tsx
@@ -1,10 +1,15 @@
 import { MarkdownContainer } from '@src/components/MarkdownContainer'
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
+import type { GetStaticProps } from 'next'
 
-const PrivacyPolicy = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const ogTitle = 'プライバシーポリシー' // ここでOGPタイトルを取得
+  return { props: { ogTitle } }
+}
+
+const PrivacyPolicy: React.FC<{ ogTitle: string }> = ({ ogTitle }) => {
   const [markdown, setMarkdown] = useState('')
-
   useEffect(() => {
     const fetchMarkdown = async () => {
       const res = await fetch('/doc/privacy-policy.md')
@@ -16,8 +21,8 @@ const PrivacyPolicy = () => {
   return (
     <>
       <Head>
-        <title>プライバシーポリシー</title>
-        <meta property="og:title" content="プライバシーポリシー" />
+        <title>{ogTitle}</title>
+        <meta property="og:title" content={ogTitle} />
       </Head>
       <MarkdownContainer markdown={markdown} />
     </>
