180e389017433877b9432e948f2dcd3f395c5c58
Yamayamaaya <yugo139013@gmail.com>
2024-01-12 22:27:18 +0900

    アンケトルGASアップデート

--- src/feature/GAS/requestForGAS.ts
+++ src/feature/GAS/requestForGAS.ts
@@ -1,6 +1,6 @@
 export const requestForGAS = (formId: string) => {
   const gasWebAppUrl =
-    'https://script.google.com/macros/s/AKfycbz6DIx2srv-7CZGDV-chEyX0mIQ0Bjftpyxwl15yYDnGw04Y6fHgrGKvqd6lJyKlq6kjQ/exec'
+    'https://script.google.com/macros/s/AKfycbzdlwrxbTb_mfCPvXqIcP5SivAvtEpeRNPGxjeiy9DZwEwJfGFv30RDZhjf-faIm67n/exec'
   const fullUrl = `${gasWebAppUrl}?formId=${formId}`
 
   // 新タブで遷移する
