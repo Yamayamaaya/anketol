16cdf5a322f15425f4d1f1076a69be998f4d8650
Yamayamaaya <yugo139013@gmail.com>
2024-05-23 16:28:02 +0900

    useSaveDataToFirestore修正

--- src/hooks/firebase/useSaveDataToFirestore.ts
+++ src/hooks/firebase/useSaveDataToFirestore.ts
@@ -9,14 +9,46 @@ import { useCallback } from 'react'
 
 export const useSaveDataToFirestore = () => {
   const saveDataToFirestore = useCallback(
-    async (collectionName: string, data: any, id?: string) => {
+    async (
+      collectionName: string,
+      data: any,
+      id?: string,
+      subCollectionName?: string,
+      subCollectionData?: any[]
+    ): Promise<string> => {
+      // 返り値の型をPromise<string>に変更
       const db = getFirestore()
-      if (id) {
-        const docRef = doc(db, collectionName, id)
-        await setDoc(docRef, data)
-      } else {
-        await addDoc(collection(db, collectionName), data)
+      let docRef
+      // 現在のタイムスタンプを取得
+      const timestamp = new Date()
+      const timestampData = {
+        createdTime: timestamp,
+        updatedTime: timestamp,
       }
+      if (id) {
+        docRef = doc(db, collectionName, id)
+        // updatedTimeのみ更新
+        await setDoc(
+          docRef,
+          { ...data, updatedTime: timestamp },
+          { merge: true }
+        )
+      } else {
+        docRef = await addDoc(collection(db, collectionName), {
+          ...timestampData,
+          ...data,
+        })
+      }
+
+      if (subCollectionName && subCollectionData) {
+        const subCollectionRef = collection(docRef, subCollectionName)
+        for (const item of subCollectionData) {
+          // サブコレクションの各ドキュメントにもタイムスタンプを設定
+          await addDoc(subCollectionRef, { ...timestampData, ...item })
+        }
+      }
+
+      return docRef.id // 保存したドキュメントのidを返す
     },
     []
   )
